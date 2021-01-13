307593180ad7dec2016a4adfc10e178fc210f32b
ManaPHP Mark <manaphp@qq.com>
2021-01-13 23:26:05 +0800

    Modified   docker/nginx/docker-entrypoint.sh

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -3,12 +3,12 @@
 #set -x
 
 echo "changing directory permission..."
-for dir in $(mount | awk '{print \$3}'); do
-  if [[ $dir =~ ^/var/www/html/ ]]; then
-    cmd="chmod -R a+rw $dir"
-    echo $cmd
-    $cmd
-  fi
+for dir in `mount|awk '{print \$3}'`;do
+   if [[ $dir =~ ^/var/www/html/ ]]; then
+	cmd="chmod -R a+rw $dir";
+	echo $cmd;
+   	$cmd;
+   fi
 done
 echo "changing directory permission done"
 
