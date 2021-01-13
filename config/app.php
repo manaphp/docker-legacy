be55332838d81658e375b1a857e01d6fd8c98e81
ManaPHP Mark <manaphp@qq.com>
2021-01-13 22:42:01 +0800

    Modified   config/app.php Modified   docker/docker-compose.yml Modified   docker/nginx/docker-entrypoint.sh

--- config/app.php
+++ config/app.php
@@ -1,27 +1,27 @@
 <?php
 
 return [
-    'id'         => 'api',
-    'env'        => env('APP_ENV', 'prod'),
-    'debug'      => env('APP_DEBUG', false),
-    'version'    => '1.1.1',
-    'timezone'   => 'PRC',
+    'id' => 'api',
+    'env' => env('APP_ENV', 'prod'),
+    'debug' => env('APP_DEBUG', false),
+    'version' => '1.1.1',
+    'timezone' => 'PRC',
     'master_key' => env('MASTER_KEY'),
-    'params'     => [],
-    'aliases'    => [],
+    'params' => [],
+    'aliases' => [],
     'components' => [
         'httpServer' => ['port' => 9501, 'max_request' => 1000000],
-        'db'         => [env('DB_URL')],
-        'redis'      => [env('REDIS_URL')],
-        'logger'     => ['level' => env('LOGGER_LEVEL', 'info')],
+        'db' => [env('DB_URL')],
+        'redis' => [env('REDIS_URL')],
+        'logger' => ['level' => env('LOGGER_LEVEL', 'info')],
     ],
-    'services'   => [],
-    'listeners'  => [],
-    'plugins'    => [
-        'debugger',
+    'services' => [],
+    'listeners' => [],
+    'plugins' => [
+        // 'debugger',
         'cors',
         //'slowlog',
         //'logger',
     ],
-    'tracers'    => env('APP_TRACERS', []),
+    'tracers' => env('APP_TRACERS', []),
 ];
